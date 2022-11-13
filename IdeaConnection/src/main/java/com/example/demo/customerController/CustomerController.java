11458f0cfc20051a048b522099a53f880262bbe5
Amey <chetu.suradkar@gmail.com>
2022-11-13 11:35:30 +0530

    add controller class

---
+++ IdeaConnection/src/main/java/com/example/demo/customerController/CustomerController.java
@@ -0,0 +1,26 @@
+package com.example.demo.customerController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.customer.CustomerIdea;
+import com.example.demo.customerSerivce.CustomerService;
+
+@RestController
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/dataSave")
+	public ResponseEntity<CustomerIdea>  dataSave(@RequestBody CustomerIdea customerIdea){
+		CustomerIdea idea=customerService.save(customerIdea);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(idea);
+		
+	}
+}
