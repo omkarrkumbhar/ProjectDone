f71c4d3d2438066d7b40f2e47f195f0487e4d20d
Amey <chetu.suradkar@gmail.com>
2022-11-13 11:02:54 +0530

    add method save

--- IdeaConnection/src/main/java/com/example/demo/customerSerivce/CustomerService.java
+++ IdeaConnection/src/main/java/com/example/demo/customerSerivce/CustomerService.java
@@ -1,5 +1,8 @@
 package com.example.demo.customerSerivce;
 
+import com.example.demo.customer.CustomerIdea;
+
 public interface CustomerService {
 
+	public CustomerIdea save(CustomerIdea customerIdea);
 }
