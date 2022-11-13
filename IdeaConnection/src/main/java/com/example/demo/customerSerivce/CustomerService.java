d38ccc91902c2443aa1342a8be96c922d7124878
Amey <chetu.suradkar@gmail.com>
2022-11-13 12:05:16 +0530

    final resolve

--- IdeaConnection/src/main/java/com/example/demo/customerSerivce/CustomerService.java
+++ IdeaConnection/src/main/java/com/example/demo/customerSerivce/CustomerService.java
@@ -5,9 +5,5 @@ import com.example.demo.customer.CustomerIdea;
 public interface CustomerService {
 
 	public CustomerIdea save(CustomerIdea customerIdea);
-<<<<<<< HEAD
-	public CustomerIdea getdta(Integer id);
-=======
 	public CustomerIdea getData(Integer id);
->>>>>>> refs/remotes/origin/main
 }
