57e33ba28c764eaf8a256469705871252e865c49
Amey <chetu.suradkar@gmail.com>
2022-11-13 11:20:20 +0530

    add repository package

---
+++ IdeaConnection/src/main/java/com/example/demo/customerRepository/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.customerRepository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.customer.CustomerIdea;
+
+@Repository
+public interface CustomerRepo extends CrudRepository<CustomerIdea, Integer> {
+
+}
