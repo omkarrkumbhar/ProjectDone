10f62471a9f6faf5c6390fc226d634593b135917
Amey <chetu.suradkar@gmail.com>
2022-11-13 12:16:49 +0530

    changes in repo

--- IdeaConnection/src/main/java/com/example/demo/customerRepository/CustomerRepo.java
+++ IdeaConnection/src/main/java/com/example/demo/customerRepository/CustomerRepo.java
@@ -1,12 +1,14 @@
 package com.example.demo.customerRepository;
 
+import java.io.Serializable;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.customer.CustomerIdea;
 
 @Repository
-public interface CustomerRepo extends CrudRepository<CustomerIdea, Integer> {
+public interface CustomerRepo extends CrudRepository<CustomerIdea, Serializable> {
 
 	public CustomerIdea getById(Integer id);
 }
