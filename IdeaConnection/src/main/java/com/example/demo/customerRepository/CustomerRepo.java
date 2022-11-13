0828efee9893aebb97d1a45ae185680950371b0f
Amey <chetu.suradkar@gmail.com>
2022-11-13 11:24:08 +0530

    add method getById

--- IdeaConnection/src/main/java/com/example/demo/customerRepository/CustomerRepo.java
+++ IdeaConnection/src/main/java/com/example/demo/customerRepository/CustomerRepo.java
@@ -8,4 +8,5 @@ import com.example.demo.customer.CustomerIdea;
 @Repository
 public interface CustomerRepo extends CrudRepository<CustomerIdea, Integer> {
 
+	public CustomerIdea getById(Integer id);
 }
